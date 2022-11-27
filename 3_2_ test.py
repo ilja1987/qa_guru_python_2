a725d479c1ab28eca7d7e23fb7cde9ff86b09e29
ПК <domniniv@mail.ru>
2022-11-27 17:05:55 +0400

    Обновили README

--- 3_2_ test.py
+++ 3_2_ test.py
@@ -12,3 +12,9 @@ def test_sl2(br_size1):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.no.text('условие задачи понято не верно'))
+
+
+def test_sl3():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('sdfq12wdqsdfsdfsdfsdfsdf').press_enter()
+    browser.element('[id="result-stats"]').should(have.text("Результатов: примерно 0"))
