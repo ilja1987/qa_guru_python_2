f839727171a2ef6507d24eb98b7ec2fc10e3d090
ПК <domniniv@mail.ru>
2022-11-27 14:12:42 +0400

    Обновили README

--- 3_2_ test.py
+++ 3_2_ test.py
@@ -1,21 +1,24 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+@pytest.fixture()
+def br_size():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
+@pytest.fixture()
+def br_size1():
+    browser.config.window_width = 1000
+    browser.config.window_height = 600
 
-def br_size(a, b):
-    browser.config.window_width = a
-    browser.config.window_height = b
 
-
-def test_sl1():
-    br_size(800, 600)
+def test_sl1(br_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
-def test_sl2():
-    br_size(1024, 800)
+def test_sl2(br_size1):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('selenoid: User-oriented Web UI browser tests in Python'))
